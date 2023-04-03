70c089f63fd3b35b3935917ad7aa2c48cc747921
Carlos Eduardo Palomo Serna <cepalomos@outlook.es>
2023-04-02 21:00:51 -0500

    rutas hechas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Landing from './Pages/Landing';
+import Home from './Pages/Home'
+import Detail from './Pages/Detail';
+import Form from './Pages/Form';
+import Error from './Pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path={"/"}>
+            <Landing />
+          </Route>
+          <Route exact path={"/home"}>
+            <Home />
+          </Route>
+          <Route exact path={"/home/:id"}>
+            <Detail />
+          </Route>
+          <Route exact path={"/create"}>
+            <Form />
+          </Route>
+          <Route path={"*"}>
+            <Error/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/Pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Landing.css';
+
+function Landing() {
+  return (
+    <div className='contenedor_principal'>
+      <button>Entrar</button>
+    </div>
+  )
+}
+
+export default Landing
